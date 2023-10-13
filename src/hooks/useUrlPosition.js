f7bf310d4635099062d1dcc79a925fe0b606490b
Niharika <debniharika3@gmail.com>
2023-10-13 23:27:50 +0530

    initial commit

---
+++ src/hooks/useUrlPosition.js
@@ -0,0 +1,11 @@
+import { useSearchParams } from "react-router-dom";
+
+export function useUrlPosition() {
+  const [searchParams] = useSearchParams();
+
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+  console.log(lat, lng);
+
+  return [lat, lng];
+}
---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import PageNav from "../components/PageNav";
+
+function PageNotFound() {
+  return (
+    <div>
+      not donf
+      <PageNav />
+    </div>
+  );
+}
+
+export default PageNotFound;
